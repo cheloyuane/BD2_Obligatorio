f66e39ed92dbed5079212d59a40581e0a58e6787
Estefany Clara <estefany.clara@correo.ucu.edu.uy>
2025-07-06 18:35:31 -0300

    se eliminaron archivos duplicados

--- backend/src/utils/logger.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.log = void 0;
-const log = (mensaje) => {
-    console.log(`[LOG]: ${mensaje}`);
-};
-exports.log = log;
